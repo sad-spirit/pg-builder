289a64527f791f28db2c9fd3c7e20a7aff3cab4c
Alexey Borzov <avb@php.net>
2020-09-19 20:56:58 +0300

    Switch to PSR-4, remove home-grown autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,6 @@
 <?php
 
-spl_autoload_register(function($class) {
-    if ('\\' == $class[0]) {
-        $class = substr($class, 1);
-    }
-
-    if (0 === strpos($class, 'sad_spirit\\pg_builder\\tests\\')
-        && file_exists($file = __DIR__. str_replace('\\', '/', substr($class, strlen('sad_spirit\\pg_builder\\tests'))) . '.php')
-    ) {
-        require_once $file;
-    }
-});
-
-if (@file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require_once __DIR__ . '/../vendor/autoload.php';
-} else {
-    require_once __DIR__ . '/../src/sad_spirit/pg_builder/autoloader.php';
-    require_once __DIR__ . '/../../pg-wrapper/src/sad_spirit/pg_wrapper/autoloader.php';
-}
+require_once __DIR__ . '/../vendor/autoload.php';
 
 date_default_timezone_set('UTC');
 
